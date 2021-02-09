8a846a9e593e362d4c3dcb8b385c8b1e60598ba2
Shmaya Frankel <Shmaya.Frankel@gmail.com>
2021-02-09 12:47:36 +0200

    test

--- src/HttpHeader.cc
+++ src/HttpHeader.cc
@@ -553,6 +553,9 @@ HttpHeader::parse(const char *header_start, size_t hdrLen, Http::ContentLengthIn
 
         if (rawTe.caseCmp("chunked") == 0) {
             ; // leave header present for chunked() method
+         else if (rawTe.caseCmp("chunked, chunked") == 0) {
+            ;// leave header present for chunked() method
+        }
         } else if (rawTe.caseCmp("identity") == 0) { // deprecated. no coding
             delById(Http::HdrType::TRANSFER_ENCODING);
         } else {
